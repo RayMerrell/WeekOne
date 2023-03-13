04943182c01d725e959d05e0b1b88b9b05f23dfe
Ray Merrell <RayMerrell@outlook.com>
2023-03-13 15:35:06 +0000

    M46Course

---
+++ Feb27/app.js
@@ -0,0 +1,12 @@
+list =[1,2,3,4,5,6];
+console.log(list);
+let shoppingList=[];
+shoppingList = [
+            ["Now", "Mr Smith", 7],
+            ["Tomorrow", "Mr Jones", 2],
+            ["Tuesday", "Tom", 3]
+];
+console.log (shoppingList[1][2]);
+
+
+
